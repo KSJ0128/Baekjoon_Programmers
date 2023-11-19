566858e7f61da837a96146fa8bc711fd7ab0cea7
Seojin <83456698+KSJ0128@users.noreply.github.com>
2023-11-19 14:54:23 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    double c,d,e;
+    cin>>a>>b;
+    c=a; d=b;
+    e=c/d;
+    cout.precision(11);
+    cout<<e;
+    return 0;
+}
