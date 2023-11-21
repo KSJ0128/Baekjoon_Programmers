6278b9f95dae34d48e710c310c1cce2b65782aa5
Seojin <83456698+KSJ0128@users.noreply.github.com>
2023-11-21 18:15:15 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    while(true)
+    {
+        cin>>a>>b;
+
+        if ((a==0)&&(b==0))
+        {
+           break;
+        }
+        cout<<(a+b)<<endl;
+    }
+}
