36b6133a8bdfb84fab523955a8a6a4118eda5fae
Seojin <83456698+KSJ0128@users.noreply.github.com>
2023-11-21 18:18:41 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int d1,d2;
+    double P;
+    cin>>d1>>d2;
+    P=d1*2+d2*3.141592*2;
+    cout.precision(12);
+    cout<<P;
+
+    return 0;
+}
