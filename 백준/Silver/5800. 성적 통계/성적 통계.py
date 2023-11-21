09a3586508a7ca58b20f3f7933c8bd553f3f2aa3
Seojin <83456698+KSJ0128@users.noreply.github.com>
2023-11-21 16:40:34 +0900

    [Silver V] Title: 성적 통계, Time: 72 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Silver/5800. 성적 통계/성적 통계.py
@@ -0,0 +1,10 @@
+K = int(input())
+
+for i in range(K):
+    nScore = list(map(int,input().split()))
+    score = sorted(nScore[1:], reverse=True)
+    gap = 0
+    for j in range(len(score)-1):
+        if gap < score[j]-score[j+1]:
+            gap = score[j]-score[j+1]
+    print(f"Class {i+1}\nMax {score[0]}, Min {score[len(score)-1]}, Largest gap {gap}")
