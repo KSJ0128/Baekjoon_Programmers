fbfc7e96a9259368831e1c7d2c011c42cab4d619
Seojin <83456698+KSJ0128@users.noreply.github.com>
2023-11-21 18:17:11 +0900

    [Bronze IV] Title: 파일 옮기기, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/11943. 파일 옮기기/파일 옮기기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A,B,C,D;
+    cin>>A>>B>>C>>D;
+
+    if(A+D>=B+C)
+        cout<<B+C;
+    else 
+        cout<<A+D;
+    return 0;
+}
