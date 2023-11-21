038c53a6dd5ddf0377daf01360c5d4bece451d2a
Seojin <83456698+KSJ0128@users.noreply.github.com>
2023-11-21 18:18:21 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A;
+    cin>>A;
+
+    if(A==0)
+    {
+        cout<<"YONSEI";
+    }
+    else if(A==1)
+    {
+        cout<<"Leading the Way to the Future";
+    }
+    return 0;
+}
