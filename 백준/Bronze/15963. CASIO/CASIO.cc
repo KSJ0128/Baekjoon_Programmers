a2996953b6578b439cefc8391e0526fa7673dd6a
Seojin <83456698+KSJ0128@users.noreply.github.com>
2023-11-21 18:18:34 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    long long A,B;
+    cin>>A>>B;
+
+    if(A==B)
+        cout<<1;
+    else
+        cout<<0;
+
+    return 0;
+}
