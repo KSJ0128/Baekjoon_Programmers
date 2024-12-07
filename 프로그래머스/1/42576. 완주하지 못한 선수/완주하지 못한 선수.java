2a16f7af9974c8d17cb24edaf9822ed875e60294
Seojin <ksj01128@naver.com>
2024-12-08 02:18:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.54 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,26 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> hash = new HashMap<String, Integer>(100000);
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
         
-        for (String complete : completion) { // 중복 반영
-            if (!hash.containsKey(complete)) {
-                hash.put(complete, 1);
-            }
-            else {
-                hash.replace(complete, hash.get(complete) + 1);
-            }
+        for (String person : participant) {
+            map.putIfAbsent(person, 0);
+            map.replace(person, map.get(person) + 1);
         }
         
-        for (String part : participant) {
-            if (hash.containsKey(part)) {
-                if (hash.get(part) == 1)
-                    hash.remove(part);
-                else
-                    hash.replace(part, hash.get(part) - 1);
-            }
-            else return part;
+        for (String person : completion) {
+            map.replace(person, map.get(person) - 1);
+            if (map.get(person) == 0)
+                map.remove(person);
         }
-        return null;
+        
+        return map.keySet().iterator().next();
     }
 }
