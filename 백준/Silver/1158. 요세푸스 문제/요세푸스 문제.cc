9fd6b75a8ed2bfacdf189d02c18dc8f731a834dd
Seojin <ksj01128@naver.com>
2025-01-15 14:56:21 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,42 +1,32 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-const int SIZE = 5000;
 int main()
 {
-    queue<int> q;
-    int josephus[SIZE];
-    int top = 0, n, k;
-    cin >> n >> k; 
-    
-    for(int i = 1; i <= n; i++)
-    {
-        q.push(i);
-    }
+	int N, K;
+	queue<int> yose;
+	cin >> N >> K;
 
-    while(!q.empty())
-    {
-        for(int i = 0; i < k-1; i++)
-        {   
-            q.push(q.front());
-            q.pop(); 
-        }
-        josephus[top++] = q.front();
-        q.pop();
-    }
+	for (int i = 1; i <= N; i++)
+		yose.push(i);
 
-    for(int i = 0; i < n; i++)
-    {
-        if(i == 0)
-        {
-            if(n == 1) // 숫자가 한 개인 경우
-                cout << "<" << josephus[0] << ">";
-            else // 숫자가 두 개 이상인 경우
-                cout << "<" << josephus[i] << ", ";
-        }
-        else if(i == n-1)
-            cout << josephus[i] << ">";
-        else 
-            cout << josephus[i] << ", ";
-    }
+	while (!yose.empty())
+	{
+		for (int i = 0; i < K - 1; i++)
+		{
+			yose.push(yose.front());
+			yose.pop();
+		}
+
+		if (yose.size() == N)
+			cout << "<";
+
+		cout << yose.front();
+		yose.pop();
+
+		if (yose.size() == 0)
+			cout << ">";
+		else
+			cout << ", ";
+	}
 }
